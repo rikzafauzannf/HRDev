b13b4d3124fd16c4f573b38f885e87692f5f6ac4
Rikza Fauzan <67640598+rikzafauzannf@users.noreply.github.com>
2024-05-24 17:07:32 +0700

    manage error

--- src/components/Cards/CardProgress.tsx
+++ src/components/Cards/CardProgress.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const CardProgress = () => {
   return (
-    <>
-        <div className="card w-full shadow-lg">
-            <div className="card-body text-center">
-                <h1>HTML</h1>
-                <div className="radial-progress" style={{"--value":70}} role="progressbar">70%</div>
-            </div>
+    <div className="card w-full shadow-lg">
+      <div className="card-body text-center">
+        <h1>HTML</h1>
+        <div 
+          className="radial-progress" 
+          style={{ '--value': 70 }} 
+          role="progressbar" 
+          aria-valuenow="70" 
+          aria-valuemin="0" 
+          aria-valuemax="100"
+        >
+          70%
         </div>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default CardProgress
+export default CardProgress;
