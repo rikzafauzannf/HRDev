8e3ff25d3a63d8c6fd4f4cbc0a478d30881edd4c
Rikza Fauzan <67640598+rikzafauzannf@users.noreply.github.com>
2024-05-24 19:58:21 +0700

    text size

--- src/components/Cards/CardProgress.tsx
+++ src/components/Cards/CardProgress.tsx
@@ -12,7 +12,7 @@ const CardProgress: React.FC<dataProps> = (props) => {
   return (
     <div className="card w-full shadow-lg bg-neutral-content/10">
       <div className="card-body text-center font-semibold hover:text-primary hover:font-bold transition-all ease-in-out delay-150 duration-300">
-        <h1 className="text-xl">{props.skill}</h1>
+        <h1 className="md:text-xl text-lg">{props.skill}</h1>
         {/* <div 
           className="radial-progress text-primary shadow-lg font-semibold" 
           style={{ '--value': props.score } as CustomCSSProperties} 
