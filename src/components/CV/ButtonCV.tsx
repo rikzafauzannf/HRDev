9b703b1230ecc004e25e98118e20cac28908e43c
Rikza Fauzan <67640598+rikzafauzannf@users.noreply.github.com>
2024-05-24 16:51:59 +0700

    button cv

--- src/components/CV/ButtonCV.tsx
+++ src/components/CV/ButtonCV.tsx
@@ -4,7 +4,7 @@ const ButtonCV = () => {
   return (
     <div>
       <a href="/cv.pdf" download>
-        <div style={{ cursor: 'pointer', padding: '10px 20px', backgroundColor: '#4CAF50', color: 'white', textAlign: 'center', borderRadius: '5px', boxShadow: '0 4px 6px rgba(0,0,0,0.1)' }}>
+        <div className="btn btn-sm btn-secondary">
           <IoMdCloudDownload /> Download CV
         </div>
       </a>
