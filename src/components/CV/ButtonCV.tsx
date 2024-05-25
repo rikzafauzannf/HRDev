26025a4e563692b65eeed74319cac4c809dae1a9
Rikza Fauzan <67640598+rikzafauzannf@users.noreply.github.com>
2024-05-25 19:13:28 +0700

    update cv

--- src/components/CV/ButtonCV.tsx
+++ src/components/CV/ButtonCV.tsx
@@ -3,7 +3,7 @@ import { IoMdCloudDownload } from "react-icons/io";
 const ButtonCV = () => {
   return (
     <div>
-      <a href="/cv.pdf" download>
+      <a href="/CV2024.pdf" download>
         <div className="btn btn-sm btn-secondary shadow-lg">
           <IoMdCloudDownload /> Download CV
         </div>
