e26c0137962a06832e5bc808cd596040554e4eb4
Rikza Fauzan <rikzarazor491@gmail.com>
2024-08-04 17:22:42 +0700

    change themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="night">
+    <html lang="en" data-theme="cupcake">
       <body className={inter.className}>{children}</body>
     </html>
   );
