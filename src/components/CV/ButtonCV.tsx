fd41e3ef0464fc6326ae7074c29f12a4ef8952ff
Rikza Fauzan <rikzarazor491@gmail.com>
2024-08-04 15:29:04 +0700

    Upload

--- src/components/CV/ButtonCV.tsx
+++ src/components/CV/ButtonCV.tsx
@@ -3,7 +3,7 @@ import { IoMdCloudDownload } from "react-icons/io";
 const ButtonCV = () => {
   return (
     <div>
-      <a href="/CV2024.pdf" download>
+      <a href="/RikzaSoftwareEngineerResume.pdf" download>
         <div className="btn btn-sm btn-secondary shadow-lg">
           <IoMdCloudDownload /> Download CV
         </div>
